a5393a68529be94b05e3f8913602018652132c1f
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-01-02 17:56:46 +0000

    Added logic to loop through houses_seed_count list and if value is under 10, convert to string with an added empty space to ensure one and two digit values are formatted the same and there is no effect to the game board

--- oware.py
+++ oware.py
@@ -4,6 +4,11 @@ playing = True
 
 while playing:
 
+    for i in range(0, len(houses_seed_count)):
+        houses_seed_count[i] = int(houses_seed_count[i])
+        if houses_seed_count[i] < 10:
+            houses_seed_count[i] = " " + str(houses_seed_count[i])
+
     print("+----+----+----+----+----+----+----+----+")
     print("|    |    |    |    |    |    |    |    |")
     print("|    |----+----+----+----+----+----|    |")
